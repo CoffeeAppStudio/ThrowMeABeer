544588956fb8004a9cd204991bf5b935c9d734b0
ronan tremoureux <ronan.tremoureux@gmail.com>
2023-05-21 14:14:58 +0200

    add thrower and object behaviour

--- Assets/ChangeSceneScript.cs
+++ Assets/ChangeSceneScript.cs
@@ -1,22 +1,85 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 using UnityEngine.SceneManagement;
 
 public class ChangeSceneScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    public GameObject ui;
+
+    private bool inUI;
+
+    public Transform StartTransformCamera;
+    
+    public Transform EndTransformCamera;
+    
+    private Vector3 CameraStartPosition;
+    private Quaternion CameraStartRotation;
+    
+    private Vector3 CameraEndPosition;
+    private Quaternion CameraEndRotation;
+    
+    private Vector3 Startposition;
+    private Quaternion StartRotation;
+    
+    private Vector3 EndPosition;
+    private Quaternion EndRotation;
+    
+    public float speed = 0.0001f;
+
+    public GameObject camera;
+
+    public bool InUI
+    {
+        get => inUI;
+        set => inUI = value;
+    }
+    
     void Start()
     {
-        
+        ui.SetActive(true);
+        CameraStartPosition = StartTransformCamera.position;
+        CameraStartRotation = StartTransformCamera.rotation;
+        CameraEndPosition = EndTransformCamera.position;
+        CameraEndRotation = EndTransformCamera.rotation;
+        EndPosition = CameraStartPosition;
+        EndRotation = CameraStartRotation;
+        InUI = true;
+        Debug.Log("started");
     }
 
-    
-    public void ChangeScene()
+    void Update()
     {
-
-        SceneManager.LoadScene("GameScene");
-        //unload the current scene
-        
+        camera.transform.position = Vector3.Lerp(camera.transform.position, EndPosition, speed*Time.deltaTime);
+        camera.transform.rotation = Quaternion.Lerp(camera.transform.rotation, EndRotation, speed*Time.deltaTime);
+    }
+    
+    public void changeScene()
+    {
+        if(InUI)
+        {
+            Debug.Log("ChangeScene to game");
+            camera.transform.position = CameraStartPosition;
+            EndPosition = CameraEndPosition;
+            camera.transform.rotation = CameraStartRotation;
+            EndRotation = CameraEndRotation;
+            ui.SetActive(false);
+            InUI = false;
+            //lerp camera to end position
+        }
+        else
+        {
+            Debug.Log("ChangeScene to UI");
+            camera.transform.position = CameraEndPosition;
+            EndPosition =  CameraStartPosition;
+            camera.transform.rotation = CameraEndRotation;
+            EndRotation =CameraStartRotation ;
+            ui.SetActive(true);
+            InUI = true;
+            
+        }
     }
 }
--- Assets/ObjectToThrowScript.cs
+++ Assets/ObjectToThrowScript.cs
@@ -1,10 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectToThrowScript : MonoBehaviour
 {
-    public float speed = 0.01f;
+    private float lifeTime = 10005f;
+    private float lifeTimer = 0f;
+    private float speed = 0.01f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,42 @@ public class ObjectToThrowScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        lifeTimer += Time.deltaTime;
         this.transform.Translate(Vector3.forward * speed);
+        if (lifeTimer > lifeTime)
+            Destroy(this.gameObject);
+        
     }
+
+    public void setSpeed(float spd)
+    {
+        speed = spd;
+        lifeTime = 0.1f/speed;
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+
+        if (other.gameObject.CompareTag("saloon"))
+        {
+            speed = 0;
+        }
+    }
+    
+    private void OnCollisionStay(Collision other)
+    {
+
+        if (other.gameObject.CompareTag("bar"))
+        {
+            if (speed > 0)
+            {
+                Debug.Log("lowering the speed");
+                speed -= 0.00001f;
+            }
+                
+        }
+    }
+
 }
